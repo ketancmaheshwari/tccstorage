b61906104eaf38746b907535a9776c6f30655ae3
ketancmaheshwari <ketancmaheshwari@gmail.com>
2014-05-28 17:31:42 -0500

    gendata systamp

--- syntheticapp/gendata/main.cpp
+++ syntheticapp/gendata/main.cpp
@@ -1,12 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "globals.h"
+#include <time.h>
+#define SIZE 256
 
 // Allocates a matrix with random float entries.
 void randomInit(float* data, int size)
 {
     for (int i = 0; i < size; ++i)
         data[i] = rand() / (float)RAND_MAX;
+}
+
+void systamp()
+{
+    char buffer[SIZE];
+    char hostname[1024];
+    char *tzone, *format;
+    time_t timestamp;
+    timestamp = time(NULL);
+    // Format string
+    format = "%a %e %b %Y %r %Z %n";
+    tzone="TZ=UTC";
+    putenv(tzone);
+    strftime(buffer, SIZE, format, localtime(&timestamp));
+    fputs(buffer, stdout);
+    gethostname(hostname, 1024);
+    puts(hostname);
 }
 
 int main(int argc, char** argv)
@@ -24,6 +44,9 @@ int main(int argc, char** argv)
     saveMatrix(argv[3], m, H, W);
 
     free(m);
+
+    systamp();
+
     return 0;
 }
 
